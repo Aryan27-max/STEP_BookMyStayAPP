62825f45e522296c48a7f5f568767754f1e63992
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-03-10 09:04:21 +0530

    UC 1

--- src/hello.java
+++
@@ -1,6 +0,0 @@
-// This is a simple Java program that prints "Hello, jytdtcxWorld!" to the console.
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
